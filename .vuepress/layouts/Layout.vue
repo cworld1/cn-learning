5dabb5a7cec8349b5151e62813f2e70c58df7507
cworld1 <cworld0@qq.com>
2023-05-13 00:47:04 +0800

    feat(vuepress): :chart_with_upwards_trend: add busuanzi support to analyse the visits

--- .vuepress/layouts/Layout.vue
+++ .vuepress/layouts/Layout.vue
@@ -2,12 +2,17 @@
   <ParentLayout>
     <template #page-bottom>
       <CommentService :darkmode="isDarkMode" />
+      <p class="my-footer" style="text-align:center">
+        &copy;2023 • <a href="https://cworld.top/">CWorld</a> • 总访问 <span id="busuanzi_value_site_pv"></span> 次
+      </p>
     </template>
   </ParentLayout>
 </template>
+
 <script setup lang="ts">
 import { onUnmounted, onMounted, ref } from "vue";
 import ParentLayout from "@vuepress/theme-default/lib/client/layouts/Layout.vue";
+// import "https://busuanzi.icodeq.com/busuanzi.pure.mini.js";
 
 const isDarkMode = ref(false);
 
@@ -25,6 +30,16 @@ onMounted(() => {
     attributeFilter: ["class"],
     attributes: true,
   });
+
+  // busuanzi
+  const script = document.createElement('script');
+  script.src = 'https://busuanzi.icodeq.com/busuanzi.pure.mini.js';
+  const footerEl = document.querySelector('.my-footer');
+  if (footerEl) {
+    footerEl.appendChild(script);
+  } else {
+    console.error('阅读量载入失败');
+  }
 
   onUnmounted(() => {
     observer.disconnect();
